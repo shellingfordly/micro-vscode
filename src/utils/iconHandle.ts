de7957faa419f72a8459dfebaa90d1c9f62b66eb
shellingfordly <shellingfordly@qq.com>
2024-03-07 10:48:19 +0800

    feat: add icons

--- src/utils/iconHandle.ts
+++ src/utils/iconHandle.ts
@@ -1,43 +1,86 @@
-export const ICON_PREFIX_MAP = {
-  cargo: "file-type-cargo",
-  "pnpm-lock": "file-type-pnpm",
-  license: "file-type-license",
-  vercel: "file-type-vercel",
-  tsconfig: "file-type-tsconfig",
-  uno: "file-type-unocss",
-  unocss: "file-type-unocss",
-  nuxt: "file-type-nuxt",
-  vite: "file-type-vite",
-  windi: "file-type-windi",
-  tailwind: "file-type-tailwind",
-  eslint: "file-type-eslint",
-  package: "file-type-npm",
-  gitignore: "file-type-git",
-  favicon: "file-type-favicon",
+const ICON_PREFIX_MAP = {
+  cargo: "cargo",
+  "pnpm-lock": "pnpm",
+  "pnpm-workspace": "pnpm",
+  npmrc: "npm",
+  package: "npm",
+  license: "license",
+  vercel: "vercel",
+  tsconfig: "tsconfig",
+  uno: "unocss",
+  unocss: "unocss",
+  nuxt: "nuxt",
+  nuxtrc: "nuxt",
+  vite: "vite",
+  windi: "windi",
+  tailwind: "tailwind",
+  eslint: "eslint",
+  gitignore: "git",
+  favicon: "favicon",
+  tauri: "tauri",
+  renovate: "renovate",
 };
 
-export const ICON_SUFFIX_MAP = {
-  js: "file-type-js",
-  ts: "file-type-typescript",
-  vue: "file-type-vue",
-  html: "file-type-html",
-  css: "file-type-postcss",
-  less: "file-type-less",
-  sass: "file-type-sass",
-  json: "file-type-json",
-  md: "file-type-markdown",
-  rs: "file-type-rust",
-  yml: "file-type-light-yaml",
-  svg: "file-type-svg",
-  png: "file-type-image",
-  jpg: "file-type-image",
-  gif: "file-type-image",
-  webp: "file-type-image",
+const ICON_SUFFIX_MAP = {
+  js: "js",
+  ts: "typescript",
+  d: "typescriptdef",
+  vue: "vue",
+  html: "html",
+  css: "postcss",
+  less: "less",
+  sass: "sass",
+  json: "json",
+  md: "markdown",
+  rs: "rust",
+  yml: "light-yaml",
+  svg: "svg",
+  png: "image",
+  jpg: "image",
+  gif: "image",
+  webp: "image",
   default: "default-file",
 };
 
 const FOLDER_MAP = {
+  vscode: "vscode",
+  util: "tools",
+  utils: "tools",
   default: "default-folder",
+  template: "template",
+  templates: "template",
+  layout: "view",
+  layouts: "view",
+  page: "view",
+  pages: "view",
+  view: "view",
+  src: "src",
+  hook: "hook",
+  component: "component",
+  components: "component",
+  style: "style",
+  styles: "style",
+  asset: "asset",
+  assets: "asset",
+  "src-tauri": "tauri",
+  public: "public",
+  node_modules: "node",
+  dist: "dist",
+  data: "db",
+  model: "model",
+  models: "model",
+  module: "module",
+  modules: "module",
+  server: "server",
+  type: "typings",
+  types: "typings",
+  github: "github",
+  nuxt: "nuxt",
+  app: "app",
+  apps: "app",
+  bin: "binary",
+  ts: "typescript",
+  typescript: "typescript",
 };
 
 export function handleFileIcon(type: string, fileName?: string) {
@@ -54,16 +97,25 @@ export function handleFileIcon(type: string, fileName?: string) {
     const prefix = list.shift() || "";
     let iconName = (ICON_PREFIX_MAP as any)[prefix];
 
-    if (iconName) return _h(iconName);
+    if (iconName) return _h("file-type-" + iconName);
 
     // 后缀
-    const suffix = list.pop() || "default";
+    const suffix = list.pop() || "";
+    if (suffix == "ts" && list.pop() == "d") {
+      return _h("file-type-" + ICON_SUFFIX_MAP.d);
+    }
+
     iconName = (ICON_SUFFIX_MAP as any)[suffix];
 
-    if (iconName) return _h(iconName);
+    if (iconName) return _h("file-type-" + iconName);
+    else return _h(ICON_SUFFIX_MAP.default);
   }
   // 文件夹
   else if (type === "dir") {
-    return _h(FOLDER_MAP.default);
+    const name = (fileName?.split(".") || []).filter((v) => v).shift() || "";
+
+    const iconName = (FOLDER_MAP as any)[name];
+    if (iconName) return _h("folder-type-" + iconName);
+    else return _h(FOLDER_MAP.default);
   }
 }
