b23bf24c8863340e82dbe87afb98224add94639d
shellingfordly <shellingfordly@qq.com>
2024-03-08 16:18:56 +0800

    fix: fix type error

--- src/monaco/setup.ts
+++ src/monaco/setup.ts
@@ -6,17 +6,13 @@ import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 
-// import vueWorker from "./vue.worker?worker";
-// import { reloadLanguageTools } from './env'
-
 export function initMonaco() {
+  // @ts-expect-error MonacoEnvironment is a global variable injected for monaco
   self.MonacoEnvironment = {
     async getWorker(_: any, label: string) {
       switch (label) {
         case "typescript":
         case "javascript":
-        // case "vue":
-        //   return new vueWorker();
 
         case "json":
           return new jsonWorker();
@@ -42,6 +38,4 @@ export function initMonaco() {
   monaco.languages.register({ id: "typescript", extensions: [".ts"] });
   monaco.languages.register({ id: "json", extensions: [".json"] });
   monaco.languages.register({ id: "html", extensions: [".html"] });
-
-  // monaco.languages.onLanguage('vue', () => reloadLanguageTools(ctx))
 }
