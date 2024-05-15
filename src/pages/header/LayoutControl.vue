d707aa58ada619e02e778d792c0400238d390a69
linken <294258474@qq.com>
2024-05-15 17:45:10 +0800

    feat: Title bar adapts to theme

--- src/pages/header/LayoutControl.vue
+++ src/pages/header/LayoutControl.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { Window } from '@tauri-apps/api/window'
-
 const appWindow = new Window('main')
 </script>
 <template>
-  <div class="h-8" style="background-color: #363636" data-tauri-drag-region flex-end>
-    <span class="mr2 op-hover i-material-symbols-light-minimize" @click="appWindow.minimize()" />
-    <span class="mr2 op-hover i-material-symbols-light-home-max-outline" @click="appWindow.toggleMaximize()" />
-    <span class="mr2 op-hover i-material-symbols-light-cancel-outline" @click="appWindow.close()" />
-  </div>
+  <n-el
+    tag="div"
+    class="h-8"
+    data-tauri-drag-region
+    flex-end
+    style="color: var(--primary-color); transition: 0.3s var(--cubic-bezier-ease-in-out)"
+  >
+    <span class="mr4 op-hover" i="material-symbols-minimize-rounded" @click="appWindow.minimize()" />
+    <span class="mr4 op-hover" i="material-symbols-chrome-maximize-outline" @click="appWindow.toggleMaximize()" />
+    <span class="mr4 op-hover" i="material-symbols-close-rounded" @click="appWindow.close()" />
+  </n-el>
 </template>
